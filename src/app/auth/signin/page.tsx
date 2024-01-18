0a36382b845e50ae278a99e86f010c66fe4dad4e
Jinhan <wlsgks846@gmail.com>
2024-01-18 20:09:34 +0900

    part 1

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,28 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../pages/api/auth/[...nextauth]";
+import { getProviders } from "next-auth/react";
+import { redirect } from "next/navigation";
+import Signin from "@/app/components/Signin";
+
+type Props = {
+  searchParams: {
+    callbackUrl: string;
+  };
+};
+export default async function SignPage({
+  searchParams: { callbackUrl },
+}: Props) {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/");
+  }
+
+  const providers = (await getProviders()) ?? {};
+
+  return (
+    <section className="flex justify-center mt-24">
+      <Signin providers={providers} callbackUrl={callbackUrl ?? "/"} />
+    </section>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <h1 className="text-gray-400">Instagram</h1>;
+}
