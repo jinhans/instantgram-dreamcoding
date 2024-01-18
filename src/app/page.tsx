e8a54bbe25db03e9986b009acd40bd74fd82f51d
Jinhan <wlsgks846@gmail.com>
2024-01-19 07:59:32 +0900

    sidebar implement and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
-export default function HomePage() {
-  return <h1 className="text-gray-400">Instagram</h1>;
+import { getServerSession } from "next-auth";
+import FollowingBar from "./components/FollowingBar";
+import PostList from "./components/PostList";
+import SideBar from "./components/SideBar";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+
+  if (!user) {
+    redirect("/auth/signin");
+  }
+  return (
+    <section className="w=full flex flex-col md:flex-row max-w-[850px] p-4">
+      <div className="w-full basis-3/4">
+        <FollowingBar />
+        <PostList />
+      </div>
+      <div className="basis-1/4">
+        <SideBar user={user} />
+      </div>
+    </section>
+  );
 }
