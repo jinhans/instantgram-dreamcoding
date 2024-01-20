b8046999eec481b23ea50e92ae3788c3bec859f4
Jinhan <wlsgks846@gmail.com>
2024-01-21 04:26:28 +0900

    Implement the following bar

--- src/app/components/FollowingBar.tsx
+++ src/app/components/FollowingBar.tsx
@@ -1,5 +1,45 @@
+"use client";
+
+import { DetailUser } from "@/model/user";
 import React from "react";
+import useSWR from "swr";
+import { PropagateLoader } from "react-spinners";
+import Avatar from "./Avatar";
+import Link from "next/link";
+import ScrollableBar from "./ui/ScrollableBar";
 
 export default function FollowingBar() {
-  return <p>FollowingBar</p>;
+  const { data, isLoading: loading, error } = useSWR<DetailUser>("/api/me");
+  // const users = data?.following;
+  // const users = undefined;
+  const users = data?.following && [
+    ...data?.following,
+    ...data?.following,
+    ...data?.following,
+  ];
+  return (
+    <section className="w-full flex justify-center item p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto">
+      {loading ? (
+        <PropagateLoader size={8} color="red" />
+      ) : (
+        (!users || users.length === 0) && <p>{`You don't have following`}</p>
+      )}
+      {users && users.length > 0 && (
+        <ScrollableBar>
+          {users.map(({ image, username }) => (
+            <Link
+              key={username}
+              className="flex flex-col items-center w-20"
+              href={`/user/${username}`}
+            >
+              <Avatar image={image} highlight />
+              <p className="w-full text-sm text-center text-ellipsis overflow-hidden">
+                {username}
+              </p>
+            </Link>
+          ))}
+        </ScrollableBar>
+      )}
+    </section>
+  );
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,13 +2,11 @@ import { addUser } from "@/service/user";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_OAUTH_ID || "",
       clientSecret: process.env.GOOGLE_OAUTH_SECRET || "",
     }),
-    // ...add more providers here
   ],
   callbacks: {
     async signIn({ user: { id, name, image, email } }) {
@@ -25,7 +23,6 @@ export const authOptions: NextAuthOptions = {
       return true;
     },
     async session({ session }) {
-      console.log(session);
       const user = session?.user;
       if (user) {
         session.user = {
